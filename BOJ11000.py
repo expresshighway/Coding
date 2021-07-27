65c6c6187c6afbe7441cb020f3e9ea4933fb8d90
expresshihway <03wow22@gmail.com>
2021-07-27 09:14:30 +0900

    [update] BOJ11000

--- BOJ11000.py
+++ BOJ11000.py
@@ -1,1 +1,22 @@
-# boj 110000
+import sys
+import heapq
+
+input = sys.stdin.readline
+table = []
+room = []
+
+N = int(input())
+table = [list(map(int, input().split())) for _ in range(N)]
+table.sort(key=lambda x: x[0])
+
+room = []
+heapq.heappush(room, table[0][1])
+
+for i in range(1, N):
+  if room[0] > table[i][0]:
+    heapq.heappush(room, table[i][1])
+  else:
+    heapq.heappop(room)
+    heapq.heappush(room, table[i][1])
+
+print(len(room))
