29dc8c545955bc2e4735b11315f39b0b02ae980d
expresshihway <03wow22@gmail.com>
2021-08-02 01:00:58 +0900

    [feat] BOJ 2668 숫자고르기

--- BOJ2668.py
+++ BOJ2668.py
@@ -1,1 +1,24 @@
-# add this page
+import sys
+input = sys.stdin.readline
+
+def dfs(v, i):
+  visited[v] = True
+  for x in arr[v]:
+    if not (visited[x]):
+      dfs(x, i)
+    elif visited[x] and x == i:
+      result.append(x)
+
+N = int(input())
+arr = [ [] for i in range(N+1)]
+for i in range(N):
+  arr[i+1].append(int(input())) 
+
+result = []
+for i in range(1, N+1):
+  visited = [False] * (N+1)
+  dfs(i, i)
+length = len(result)
+print(length)
+for i in range(length):
+  print(result[i])
