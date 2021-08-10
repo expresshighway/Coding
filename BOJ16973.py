a847c90a9505157953c422f3deef45653c18dcc4
expresshihway <03wow22@gmail.com>
2021-08-10 11:33:15 +0900

    [feat] BOJ 2108 통계학

--- BOJ16973.py
+++ BOJ16973.py
@@ -1,3 +1,43 @@
+import sys
+
+input = sys.stdin.readline
+N, M = map(int, input().split())
+
+MAP = []
+for _ in range(N):
+    row = list(map(int, input().split()))
+    MAP.append(row)
+
+H, W, Sr, Sc, Fr, Fc = map(int, input().split())
+
+#좌표 시작 기준점이 1,1 이므로 1씩 빼준다. 
+Sr, Sc, Fr, Fc = Sr-1, Sc-1, Fr-1, Fc-1 
+
+#발견 여부를 기록할 리스트
+visited = [[0 for i in range(M)] for i in range(N)]
+
+#직사각형 내부에 벽이 있으면 안되므로 벽 위치를 기록해준다.
+walls =[]
+for row in range(N):
+    for col in range(M):
+        if MAP[row][col] ==1:
+            walls.append((row,col,))
+            
+#새롭게 이동된 시작으로 직사각형이 움직일 수 있는지 없는지를 판단
+def isSatisfied(nrow, ncol):
+	
+    #이동한 시작점을 기준으로 했을 때 직사각형이 맵 밖으로 넘어가면 안된다.
+    if nrow + H -1 >= N or ncol + W -1 >= M:
+        return False
+
+	#직사각형 내부에 벽이 있으면 안된다.
+    for w_row, w_col in walls:
+        if nrow <= w_row < nrow + H and ncol <= w_col < ncol + W:
+            return False
+    return True
+
+
+#BFS
 def bfs():
     queue =[]
     #직사각형의 시작점과 이동 횟수를 넣어준다.
---
+++ BOJ2108.py
@@ -0,0 +1,27 @@
+import sys
+from collections import Counter
+
+input = sys.stdin.readline
+
+N = int(input())
+num = [int(input()) for _ in range(N)]
+num.sort()
+count = Counter(num).most_common()
+
+result1 = round(sum(num)/N)
+result2 = num[N//2]
+
+if N > 1:
+  if count[0][1] == count[1][1]:
+    result3 = count[1][0]
+  else:
+    result3 = count[0][0]
+else:
+  print(num[0])
+  
+result4 = num[-1] - num[0]
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
