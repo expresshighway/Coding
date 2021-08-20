51f2f895f16630ae40fe11976242142bd9d50a95
expresshihway <03wow22@gmail.com>
2021-08-20 14:25:04 +0900

    [update] BOJ2663 색종이 문제 완료

--- BOJ2563.py
+++ BOJ2563.py
@@ -1,1 +1,17 @@
-#add 색종이
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+array =[[0 for _ in range(101)] for _ in range(101)]
+
+for _ in range(N):
+  x, y = map(int, input().split())
+  for i in range(x, x+10):
+    for j in range(y, y+10):
+      array[i][j] = 1
+
+answer = 0
+for num in array:
+  answer = answer + num.count(1);
+
+print(answer)
