6b3525ee585704fddf534c5dad354ca1da889e60
expresshihway <03wow22@gmail.com>
2021-09-02 23:24:07 +0900

    feat: boj2217

--- BOJ1427.py
+++ BOJ1427.py
@@ -1,14 +1,14 @@
-import sys
+# import sys
 
-input = sys.stdin.readline
-N = input()
-arr = []
+# input = sys.stdin.readline
+# N = input()
+# arr = []
 
-for i in N:
-  arr.append(i)
+# for i in N:
+#   arr.append(i)
 
-for i in sorted(arr, reverse=True):
-  print(i, end='')
+# for i in sorted(arr, reverse=True):
+#   print(i, end='')
 
 
 print(''.join(reversed(sorted(input()))))
